85970f21adacb4d220db422d75e8c181368fbebb
Mayke <mayke.furtado@arcom.com.br>
2021-12-30 15:30:13 -0300

    feat: page lancamentos

--- src/pages/App/componentes/Lancamento/index.js
+++ src/pages/App/componentes/Lancamento/index.js
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
-import {Fab} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import React, {useState} from "react";
 import {
     Button,
-    Paper,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Fab, FormControl, InputLabel, MenuItem,
+    Paper, Select,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
     TableRow,
+    TextField
 } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import EditIcon from '@mui/icons-material/Edit';
 
 
 const Componente = () => {
 
     const [lancamentos, setLancamentos] = useState([]);
+    const [operacao, setOperacao] = useState('');
+    const [openForm, setOpenForm] = useState(false);
 
-    return(
+    return (
         <>
             <TableContainer style={{marginTop: 10}} component={Paper}>
                 <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
@@ -43,17 +50,19 @@ const Componente = () => {
                                 </TableCell>
                                 <TableCell align="right">{row.operacao}</TableCell>
                                 <TableCell align="right">{row.qtd}</TableCell>
-                                <TableCell align="right">{row.data}</TableCell>                        
+                                <TableCell align="right">{row.data}</TableCell>
                                 <TableCell align="right">
-                                    <Button onClick={() => {}}>
+                                    <Button onClick={() => {
+                                    }}>
                                         <EditIcon/>
                                     </Button>
                                 </TableCell>
                                 <TableCell align="right">
-                                    <Button onClick={()=> {}}>
+                                    <Button onClick={() => {
+                                    }}>
                                         <EditIcon/>
                                     </Button>
-                                </TableCell>                            
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
@@ -67,11 +76,57 @@ const Componente = () => {
                      right: 16,
                  }}
                  onClick={() => {
-
+                     setOpenForm(true);
                  }}
             >
                 <AddIcon/>
             </Fab>
+            <Dialog open={openForm} >
+                <DialogTitle>Novo Lancamento</DialogTitle>
+                <DialogContent>
+                    <div>
+                        <TextField
+                            label="Ativo"
+                            type="text"
+                            variant="standard"
+                        />
+                        <div>
+                            <FormControl>
+                                <InputLabel id="demo-simple-select-label">Operação</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={operacao}
+                                    label="Operação"
+                                    onChange={(event) => {setOperacao(event.target.value)}}
+                                >
+                                    <MenuItem value={'Compra'}>Compra</MenuItem>
+                                    <MenuItem value={'Venda'}>Venda</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
+                        <TextField
+                            label="Quantidade"
+                            type="number"
+                            variant="standard"
+                        />
+                        <TextField
+                            id="date"
+                            label="Birthday"
+                            type="date"
+                            defaultValue="2017-05-24"
+                            sx={{ width: 220 }}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => {setOpenForm(false)}}>Sair</Button>
+                    <Button onClick={() => {}}>Salvar</Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 };
